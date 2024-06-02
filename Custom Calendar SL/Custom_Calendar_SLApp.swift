6b51a6b02ef502586402577ee89486a8242e829c
Mark J Perryman <tazmancoder@gmail.com>
2024-06-02 08:50:18 -0600

    Initial Commit

---
+++ Custom Calendar SL/Custom_Calendar_SLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Calendar_SLApp.swift
+//  Custom Calendar SL
+//
+//  Created by Mark Perryman on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Calendar_SLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
